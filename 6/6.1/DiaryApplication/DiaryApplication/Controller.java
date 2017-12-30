daba5cd2ce341cf20ea953d4032ba13c444075d9
ShaneSloth <skeletalorigin@gmail.com>
2017-12-30 23:57:33 +0000

    Programming Update Batch #3
    Uploaded 4.1 through 6.1

---
+++ 6/6.1/DiaryApplication/DiaryApplication/Controller.java
@@ -0,0 +1,31 @@
+package DiaryApplication;
+
+
+/*
+ * Deal with DiaryGUI interactions from user
+ */
+public class Controller
+{
+  private ModelOfDiary diary;
+  private ViewOfDiary  view;
+
+  public Controller( ModelOfDiary aModelOfDiary, ViewOfDiary aViewOfDiary )
+  {
+    diary = aModelOfDiary;
+    view  = aViewOfDiary;
+    view.setController( this );
+  }
+
+  public void process( String buttonName, String message )
+  {
+    if ( message != null )
+      diary.saveMessageForToday( message );
+    diary.changeDate( buttonName );
+  }
+
+  public void close()
+  {
+    diary.close();
+  }
+
+}
